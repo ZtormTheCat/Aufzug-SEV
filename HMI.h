3078e190272df921f2f94c6a5d5c571c6586110f
Tim Goehringer <tim.ghr@pm.me>
2023-06-12 14:55:28 +0200

    Added sensor to hmi

--- HMI.h
+++ HMI.h
@@ -18,31 +18,42 @@ class HMI
     UserLED LEDControl = UserLED();
     //UserLED LEDControl();
 public:
-    
+  /**
+  * @brief Print desired message to console
+  * @param message message to be output
+  */
   void printMessage(std::string message){
       std::cout << message << std::endl;
     }
     
-    char getUserInput(){
-      while(1){
-        std::string input = "";
-        if(Def::op_mode::enable == 0){
-          std::cin >> input;
-        };
-        if(input == "X" || input == "x" || ButtonControl.Pressed() == 'X')
-        {
-          printMessage("Error");
-          return('X');
-        }
-        else if(input == "B" || ButtonControl.Pressed() == 'B'){
-          printMessage("Start");
-          return('B');
-        }
-        else if(input == "F" || ButtonControl.Pressed() == 'F'){
-          printMessage("Select");
-          return('F');
-        }  
+  /**
+  * @brief Get the user input from either keyboard or HW Buttons
+  * @return The user input char
+  */
+  char getUserInput(){
+    while(1){
+      std::string input = "";
+      if(Def::op_mode::enable == 0){
+        std::cin >> input;
+      };
+      if(input == "X" || input == "x" || ButtonControl.Pressed() == 'X')
+      {
+        printMessage("Error");
+        return('X');
       }
+      else if(input == "B" || ButtonControl.Pressed() == 'B'){
+        printMessage("Start");
+        return('B');
+      }
+      else if(input == "F" || ButtonControl.Pressed() == 'F'){
+        printMessage("Select");
+        return('F');
+      }
+      else if (input == "S" || ButtonControl.Pressed() == 'S'){
+        printMessage("Sensor");
+        return('S');
+      }
+    }
     }
     
   void controlLED(uint8_t value) {
