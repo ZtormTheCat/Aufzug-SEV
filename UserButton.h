29e9a2c2fde85d273f96b946213e2ca8a1ce7e76
Dominik Podsada <dominik.podsada@gmail.com>
2023-06-13 14:02:41 +0200

    Deleted the Code from UserButton.h

--- UserButton.h
+++ UserButton.h
@@ -19,20 +19,13 @@ private:
   *
   */
   void Init()
-  {
-    ButtonPort = new GPIO(ButtonPortAddr);
-    ButtonPort->configurePort(0xFFFF);
-  }
   
 public:
   /**
   * @brief Construct a new UserButton object on port B
   *
   */
-  UserButton():ButtonPortAddr(Def::enumPort::PortB)
-  {
-    Init();
-  }
+  UserButton()
   
   /**
   * @brief Read the input signal of the pin on the port that is at ButtonPortAddr
@@ -40,10 +33,6 @@ public:
   * @param pin The pin number of the input pin to read (value from 0 to 15)
   */
   bool readButtonState(uint16_t pin)
-  {
-    uint16_t readMask = 0x01 << pin;
-    return (bool)(ButtonPort->getPort() & readMask);
-  }
   
   /**
   * @brief Checks wether one of the user-input buttons or the sensor button is pressed
@@ -51,23 +40,5 @@ public:
   * 'S' for Sensor, 'X' for Start and Select, 'B' for Start and 'F' for Select.
   */
   char Pressed()
-  {
-    //check current state of the user-input
-    bool StartButton  = readButtonState(3);
-    bool SelectButton = readButtonState(5);
-    bool Sensor       = readButtonState(10);
-    //bool SensorButton = readButtonState(10);
-    
-    if      (Sensor)
-      return 'S';
-    else if (StartButton && SelectButton)
-      return 'X';
-    else if (StartButton && !SelectButton)
-      return 'B';
-    else if (!StartButton && SelectButton)
-      return 'F';
-    else
-      return 0;
-  }
 };
 #endif
