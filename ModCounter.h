312369d971c8fbfa3901e12ec5631b1b47d1f672
Dominik Podsada <dominik.podsada@gmail.com>
2023-06-13 16:50:56 +0200

    added documentation to ModCounter.h

--- ModCounter.h
+++ ModCounter.h
@@ -1,16 +1,29 @@
-//ModCounter.h
 #include "counter.h"
 
 #ifndef _MODCOUNTER_
 #define _MODCOUNTER_
+
+/**
+* @brief Class to demonstrate a modulo counter which inheritates from the Counter-Class
+*/
 class ModCounter : public Counter
 {
 private:
-  int modVal;
+  int modVal; //< the number which is used to module divide the count value
   
 public:
+  /**
+  * @brief Creates a ModCounter-Object if no start modVal is given to the constructor
+  * the start value is 4
+  *
+  * @param v is the value by which the count is modulo divided at increment
+  */
   ModCounter(int _modVal = 4) : modVal(_modVal) {}
   
+  /**
+  * @brief Increments the counter value and modulo divides it by modVal
+  *
+  */
   void Inc() {count = (count + 1)%modVal;}
 
 };
