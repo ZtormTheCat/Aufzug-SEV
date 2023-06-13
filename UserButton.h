53196d26626a5efe60ad46416a94006aec173354
Dominik Podsada <dominik.podsada@gmail.com>
2023-06-13 13:40:03 +0200

    Doxygen Styleguide Comments to UserButton.h

--- UserButton.h
+++ UserButton.h
@@ -4,37 +4,52 @@
 
 #ifndef _USERBUTTON_
 #define _USERBUTTON_
+
+/**
+* @brief Class to read the buttons using the GPIO-CLass
+*/
 class UserButton
 {
 private:
-  unsigned long ButtonPortAddr;
-  GPIO *ButtonPort;
-  
-  const int debounceDelay = 50;
-  unsigned long debounceCounter;
-  uint16_t buttonState;
-  uint16_t lastButtonState;
+  unsigned long ButtonPortAddr; //< Is the port-adress of the button port
+  GPIO *ButtonPort; //< Reference to GPIO-Class
   
 public:
-  //TODO: change address to PortB
+  /**
+  * @brief Construct a new UserButton object on port B
+  *
+  */
   UserButton():ButtonPortAddr(Def::enumPort::PortB)
   {
     ButtonPort = new GPIO(ButtonPortAddr);
     //Init();
   }
   
+  /**
+  * @brief Creates a GPIO-Object and configures the port to an input-port
+  *
+  */
   void Init()
   {
     ButtonPort->configurePort(0xFFFF);
   }
   
+  /**
+  * @brief Read the input signal of the pin on the port that is at ButtonPortAddr
+  *
+  * @param pin The pin number of the input pin to read (value from 0 to 15)
+  */
   bool readButtonState(uint16_t pin)
   {
     uint16_t readMask = 0x01 << pin;
     return (bool)(ButtonPort->getPort() & readMask);
   }
   
-  
+  /**
+  * @brief Checks wether one of the user-input buttons or the sensor button is pressed
+  * and returns a char if at least one is pressed.
+  * 'S' for Sensor, 'X' for Start and Select, 'B' for Start and 'F' for Select.
+  */
   char Pressed()
   {
     //check current state of the user-input
