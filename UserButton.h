ae8c9f459bb634c401ef50748d276a575c9c2fbf
Tim Goehringer <tim.ghr@pm.me>
2023-06-13 18:03:52 +0200

    Added semicolons to work with doxygen

--- UserButton.h
+++ UserButton.h
@@ -16,26 +16,26 @@ class UserButton
     /**
     * @brief Creates a GPIO-Object and configures the port to an input-port
     */
-    void Init()
+    void Init();
 
   public:
     /**
     * @brief Construct a new UserButton object on port B
     */
-    UserButton()
+    UserButton();
 
     /**
     * @brief Read the input signal of the pin on the port that is at ButtonPortAddr
     * @param pin The pin number of the input pin to read (value from 0 to 15)
     * @return false if a LOW signal is noticed at this pin, true if it's a HIGH signal
     */
-    bool readButtonState(uint16_t pin)
+    bool readButtonState(uint16_t pin);
 
     /**
     * @brief Checks wether one of the user-input buttons or the sensor button is pressed
     * and returns a char if at least one is pressed.
     * @return 'S' for Sensor, 'X' for Start and Select, 'B' for Start and 'F' for Select.
     */
-    char Pressed()
+    char Pressed();
 };
 #endif
