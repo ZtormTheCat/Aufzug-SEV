4e3e08b588b07c3b652d9a34194b9bfaec0a3744
Dominik Podsada <dominik.podsada@gmail.com>
2023-06-13 14:01:38 +0200

    Included a UserButton.cpp and completed it

---
+++ UserButton.cpp
@@ -0,0 +1,37 @@
+#include "UserButton.h"
+
+UserButton::UserButton():ButtonPortAddr(Def::enumPort::PortB)
+{
+    Init();
+}
+
+void UserButton::Init()
+{
+    ButtonPort = new GPIO(ButtonPortAddr);
+    ButtonPort->configurePort(0xFFFF);
+}
+
+bool UserButton::readButtonState(uint16_t pin)
+{
+    uint16_t readMask = 0x01 << pin;
+    return (bool)(ButtonPort->getPort() & readMask);
+}
+
+char UserButton::Pressed()
+{
+    //check current state of the user-input
+    bool StartButton  = readButtonState(3);
+    bool SelectButton = readButtonState(5);
+    bool Sensor       = readButtonState(10);
+
+    if      (Sensor)
+        return 'S';
+    else if (StartButton && SelectButton)
+        return 'X';
+    else if (StartButton && !SelectButton)
+        return 'B';
+    else if (!StartButton && SelectButton)
+        return 'F';
+    else
+        return 0;
+}
